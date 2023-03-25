aac5139983f7e5a2e1d0d91b5aeebb446db64143
Arsen <arsenok@greetingsisland.com>
2023-03-26 00:31:28 +0300

    commit

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const mario = document.querySelector('.mario')
 const canvas = document.querySelector('.canvas')
 
+let docLoaded = false;
+let bgMusic = null;
+
+const HOOKAH_EFFECT_MS = 11000;
+let underHookahEffect = false;
+
 let playing = true;
 let tiresCount = Number.parseInt(localStorage.getItem("tires")) || 0;
 let gameSpeed = 300;
@@ -11,17 +17,24 @@ const tiresCountElem = document.querySelector('.tires-count');
 tiresCountElem.innerHTML = "Tires count: " + tiresCount;
 
 document.addEventListener('keydown', e => {
-    if (!playing) location.reload();
+    if (!playing) {
+        location.reload()
+    }
+
+    if (!docLoaded) {
+        bgMusic = new Audio('./audio/music/tyagi-bg.mp3');
+        bgMusic.play();
+
+        docLoaded = true;
+    }
 
     if (!mario.className.includes('jump')) {
         audio = new Audio('https://itc.yananas.com/git/mario/assets/jump.wav');
         audio.play();
-
-        mario.classList.add('jump');
-   
+        mario.classList.add('jump')
         setTimeout(() => {
-            mario.classList.remove('jump');
-        }, 500);
+            mario.classList.remove('jump')
+        }, 500)
     }
 })
 
@@ -37,35 +50,52 @@ const checkForCollision = setInterval(() => {
         if (pipe.offsetLeft < -75) pipe.remove()
 
         if (pipePosition < 70 && pipePosition > 0 && marioPosition < 65) {
-            pipe.style.animation = 'none'
-            pipe.style.left = pipePosition + 'px'
-
-            mario.style.animation = 'none'
-            mario.style.bottom = marioPosition + 'px'
-            mario.src = './images/dead.png'
-            mario.style.width = '45px'
-            mario.style.marginLeft = '25px'
-
-            document.querySelectorAll('.pipe').forEach(pipe => {
-                const pipePosition = pipe.offsetLeft
+            if (!underHookahEffect) {
                 pipe.style.animation = 'none'
                 pipe.style.left = pipePosition + 'px'
-            });
 
-            document.querySelectorAll('.tire').forEach(tire => {
-                const tirePosition = tire.offsetLeft
-                tire.style.animation = 'none'
-                tire.style.left = tirePosition + 'px'
-            })
+                mario.style.animation = 'none'
+                mario.style.bottom = marioPosition + 'px'
+                mario.src = './images/dead.png'
+                mario.style.width = '45px'
+                mario.style.marginLeft = '25px'
 
-            audio = new Audio('https://itc.yananas.com/git/mario/assets/die.wav');
-            audio.play();
-            music.pause();
+                document.querySelectorAll('.pipe').forEach(pipe => {
+                    const pipePosition = pipe.offsetLeft
+                    pipe.style.animation = 'none'
+                    pipe.style.left = pipePosition + 'px'
+                });
 
-            playing = false;
-            clearInterval(checkForCollision)
-            clearInterval(newPipes)
-            clearInterval(newTires)
+                document.querySelectorAll('.tire').forEach(tire => {
+                    const tirePosition = tire.offsetLeft
+                    tire.style.animation = 'none'
+                    tire.style.left = tirePosition + 'px'
+                })
+
+                document.querySelectorAll('.hookah').forEach(hookah => {
+                    const hookahPosition = hookah.offsetLeft
+                    hookah.style.animation = 'none'
+                    hookah.style.left = hookahPosition + 'px'
+                })
+
+                audio = new Audio('https://itc.yananas.com/git/mario/assets/die.wav');
+                audio.play();
+                music.pause();
+
+                playing = false;
+                clearInterval(checkForCollision)
+                clearInterval(newPipes)
+                clearInterval(newTires)
+                clearInterval(newHookahs)
+            } else {
+                function generateRandomInteger(min, max) {
+                    return Math.floor(min + Math.random()*(max - min + 1))
+                }
+                pipe.style.transform = `translate(3000px, ${generateRandomInteger(-1000, 0)}px)`;
+                setTimeout(() => {
+                    pipe.remove();
+                }, 500);
+            }
         }
     });
 
@@ -87,6 +117,43 @@ const checkForCollision = setInterval(() => {
         }
     });
 
+    document.querySelectorAll('.hookah').forEach(hookah => {
+        const tirePosition = hookah.offsetLeft
+        if (hookah.offsetLeft < 5) hookah.remove()
+
+        if (tirePosition < 70 && tirePosition > 20 && marioPosition < 65) {
+            bgMusic.pause();
+            hookah.remove();
+            tiresCount++;
+
+            tiresCountElem.innerHTML = "Tires count: " + tiresCount;
+            audio = new Audio('./audio/music/barhat.mp3');
+            audio.play();
+
+            document.querySelectorAll('.hookah').forEach(hookah => hookah.remove());
+
+            underHookahEffect = true;
+
+            mario.src = './images/pikachu-running.gif'
+
+            document.querySelector('.hookah-effect').style.display = 'flex';
+
+            setTimeout(() => {
+                document.querySelector('.hookah-effect').style.display = 'none';
+                mario.src = './images/mario.gif';
+                audio.pause();
+                bgMusic.play();
+                underHookahEffect = false;
+            }, HOOKAH_EFFECT_MS);
+
+            // document.querySelectorAll('.pipe').forEach(pipe => {
+            //     const pipePosition = pipe.offsetLeft
+            //     tire.style.animation = 'none'
+            //     tire.style.left = pipePosition + 'px'
+            // })
+        }
+    });
+
 }, 10)
 
 const newPipes = setInterval(() => {
@@ -99,6 +166,17 @@ const newPipes = setInterval(() => {
         canvas.appendChild(newPipe)
     }
 }, 500)
+
+const newHookahs = setInterval(() => {
+    if (Math.random() < .3 && !underHookahEffect) {
+        const newPipe = document.createElement('img')
+        newPipe.src = './images/hookah.png'
+        newPipe.classList.add('hookah')
+        newPipe.style.animationDuration = window.innerWidth / gameSpeed + 's'
+
+        canvas.appendChild(newPipe)
+    }
+}, 2000)
 
 const newTires = setInterval(() => {
     if (Math.random() < .5) {
